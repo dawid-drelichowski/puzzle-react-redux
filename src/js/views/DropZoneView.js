f2fa60f1cc938524a072444ae229f02cf44a6c6a
Dawid <dawid.drelichowski@gmail.com>
2016-10-04 23:08:33 +0200

    onDragStart & onDragOver event handlers addition.

--- src/js/views/DropZoneView.js
+++ src/js/views/DropZoneView.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default class extends React.Component {
   static propTypes = {piecesCount: React.PropTypes.number.isRequired};
+  handleDragOver(event) {
+    console.log(event.target.dataset.valid);
+  }
   render() {
     const pieces = new Array(this.props.piecesCount).fill(true);
 
     return <div className="col-lg-5 label label-success">
       <ul className="grid label-success">
         {pieces.map((value, index) => {
-          return <li key={index} data-valid={index + 1}>{index + 1}</li>;
+          return <li key={index} data-valid={index + 1} onDragOver={this.handleDragOver}>{index + 1}</li>;
         })}
       </ul>
     </div>;
